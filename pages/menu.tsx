698cfb72e2f7f7971e059750f9d3d60031a10831
reyes-ale <bangtan.sonyeondan13061306@gmail.com>
2025-11-22 23:55:21 -0600

    menu terminado

--- pages/menu.tsx
+++ pages/menu.tsx
@@ -13,38 +13,41 @@ type Plato = {
 // Datos de categorías
 const categoriasData = {
   picar: [
-    { nombre: "Nachos 'El Ejecutivo'", precio: "L 150", descripcion: "Tortillas fritas con frijoles fritos, salsa jalapeña y trozos de res especial y chimol.", imagen: "/nachos.jpg" },
+    { nombre: "Nachos 'El Ejecutivo'", precio: "L 150", descripcion: "Tortillas fritas con frijoles fritos, queso mozarella, salsa jalapeña, trozos de res especial y chimol.", imagen: "/nachos.jpg" },
     { nombre: "Picadero", precio: "L 250", descripcion: "Res, cerdo y chorizo BBQ acompañado con deliciosos frijoles, trocitos de queso, tortillas fritas y chimol.", imagen: "/picadero.png" },
     { nombre: "Alitas", precio: "L 185 – 550", descripcion: "14 unidades L. 185 \n 30 unidades L. 320 \n 50 unidades L. 550 \n 6 alas + papas L. 90", imagen: "/alitass.jpg" },
     { nombre: "Dados de Queso", precio: "L. 150", descripcion: "Deliciosos trozos de queso a la parrilla.", imagen: "/dados.jpg" },
-    { nombre: "Papas con Queso", precio: "L. 150", descripcion: "Deliciosas papas fritas sazonadas con queso cheddar y tocino.", imagen: "/paps.jpg" },
-    { nombre: "Anafre", precio: "L. 150", descripcion: "Deliciosos trozos de queso a la parrilla.", imagen: "/dados.jpg" },
+    { nombre: "Papas con Queso", precio: "L. 150", descripcion: "Deliciosas papas fritas sazonadas con queso cheddar y tocino.", imagen: "/papasqueso.png" },
+    { nombre: "Anafre", precio: "L. 150", descripcion: "Nuestros deliciosos frijoles de la casa, compañados de: \nMantequilla L. 120 \n Quesillo L. 150 \n Chorizo y Quesillo L. 180", imagen: "/anafre.webp" },
   ],
   hamburguesas: [
     { nombre: "Hamburguesa La Oficina", precio: "L 150", descripcion: "Carne sobre lechuga, tomate, cebolla caramelizada, queso cheddar, aderezo de la casa, tocino y papa.", imagen: "/hambulaofi.jpg" },
-    { nombre: "Hamburguesa La Jefa", precio: "L 150", descripcion: "Pechuga de pollo empanizada sobre lechuga y tomate, bañada en salsa búfalo.", imagen: "/hamburguesa2.jpg" },
+    { nombre: "Hamburguesa La Jefa", precio: "L 150", descripcion: "Pechuga de pollo empanizada sobre lechuga y tomate, bañada en salsa búfalo.", imagen: "/lajefa.jpg" },
   ],
   otros: [
-    { nombre: "Philly Steak", precio: "L 150", descripcion: "Pan con salsa philly, tiras de carne de res, cebolla y chile verde con papas fritas y refresco.", imagen: "/philly.jpg" },
+    { nombre: "Philly Steak", precio: "L 150", descripcion: "Pan con salsa philly, tiras de carne de res, cebolla y chile verde con papas fritas y refresco.", imagen: "/phillyy.jpg" },
     { nombre: "Club Sandwich", precio: "L 150", descripcion: "Clásico sandwich con jamón de cerdo, jamon de pollo, queso suizo y cheddar, lechuga, tomate y tocino con papas fritas y refresco.", imagen: "/clubsand.jpg" },
-     { nombre: "Sandwich de pollo", precio: "L 150", descripcion: "Emparedado de pollo a la plancha, lechuga, tomate, queso cheddar, chile morrón y salsa rosada con papas fritas y refresco.", imagen: "/clubsand.jpg" },
-    
-    
-
-    { nombre: "Pizza 'La Oficina'", precio: "L 150", descripcion: "Pizza con salsa especial, queso y un ingrediente.", imagen: "/pizza1.jpg" },
-    { nombre: "Pizza 'Full'", precio: "L 200", descripcion: "Pizza con salsa especial y 3 ingredientes.", imagen: "/pizza2.jpg" },
+    { nombre: "Sandwich de pollo", precio: "L 150", descripcion: "Emparedado de pollo a la plancha, lechuga, tomate, queso cheddar, chile morrón y salsa rosada con papas fritas y refresco.", imagen: "/sandpollo.jpg" },
+    { nombre: "Tacos", precio: "L 150", descripcion: "Deliciosos tacos con ensalda de repollo, salsa y queso. Elije entre: \n 3 tacos de pollo L. 100 \n 3 tacos de res L. 100", imagen: "/tacos.jpg" },
+    { nombre: "Empanadas La Oficina", precio: "L 70", descripcion: "Orden de 2 empanadas de carne.", imagen: "/empanadas.jpg" },
+    { nombre: "Chuleta con Tajadas", precio: "L 110", descripcion: "Chuleta a la parrilla acompañada de tajadas de guineo verde.", imagen: "/chuleta.jpg" },
+    { nombre: "Pollo Chuco", precio: "L 110", descripcion: "Pollo empanizado acompañado de tajadas de guineo verde y deliciosa salsa especial La Oficina.", imagen: "/pollochuco.jpg" },
+    { nombre: "Pizza 'La Oficina'", precio: "L 150", descripcion: "Pizza con salsa especial, queso y un ingrediente.", imagen: "/pizza.avif" },
+    { nombre: "Pizza 'Full'", precio: "L 200", descripcion: "Pizza con salsa especial y 3 ingredientes.", imagen: "/pizza3.jpg" },
+    { nombre: "Jugos Naturales", precio: "L 65", descripcion: "Refrescantes jugos 100% naturales, preparados con frutas frescas de temporada.", imagen: "/jugos.jpg" }
   ],
   mariscos: [
-    { nombre: "Camarones Empanizados", precio: "L 260", descripcion: "8 camarones jumbo acompañados de puré de papa y ensalada coleslaw.", imagen: "/camarones1.jpg" },
-    { nombre: "Camarones al Ajillo", precio: "L 260", descripcion: "8 camarones jumbo acompañados de puré de papa y ensalada coleslaw.", imagen: "/camarones2.jpg" },
-    { nombre: "Camarones Endiablados", precio: "L 260", descripcion: "8 camarones jumbo acompañados ed puré de papa y ensalada coleslaw.", imagen: "/camarones3.jpg" },
-    { nombre: "Mar y Tierra", precio: "L 350", descripcion: "6oz de carne de res acompañada con camarones y puré de papa.", imagen: "/marytierra.jpg" },
-     { nombre: "Cóctel de Camarones", precio: "L 260", descripcion: "", imagen: "/camarones3.jpg" }
+    { nombre: "Camarones Empanizados", precio: "L 260", descripcion: "8 camarones jumbo acompañados de puré de papa y ensalada coleslaw.", imagen: "/empanizados.jpg" },
+    { nombre: "Camarones al Ajillo", precio: "L 260", descripcion: "8 camarones jumbo acompañados de puré de papa y ensalada coleslaw.", imagen: "/ajillo.avif" },
+    { nombre: "Camarones Endiablados", precio: "L 260", descripcion: "8 camarones jumbo acompañados ed puré de papa y ensalada coleslaw.", imagen: "/endiablados.jpg" },
+    { nombre: "Mar y Tierra", precio: "L 350", descripcion: "6oz de carne de res acompañada con camarones y puré de papa.", imagen: "/martierra.jpg" },
+     { nombre: "Cóctel de Camarones", precio: "L 260", descripcion: "Delicioso cóctel preparado con camarones frescos, bañados en una mezcla especial de salsa rosada, limón y especias.", imagen: "/coctel.jpg" }
   ],
   sopas: [
-    { nombre: "Sopa del día", precio: "L 120", descripcion: "Pollo / Mondongo / Capirotada / Frijoles (según el día).", imagen: "/sopa1.jpg" },
-    { nombre: "Sopa de Caracol", precio: "L 150", descripcion: "Clásica sopa típica hondureña de caracol.", imagen: "/caracol.jpg" },
+    { nombre: "Sopa del día", precio: "L 120", descripcion: "Pollo / Mondongo / Capirotada / Frijoles (según el día).", imagen: "/sopaa.jpg" },
+    { nombre: "Sopa de Caracol", precio: "L 150", descripcion: "Clásica sopa típica hondureña de caracol.", imagen: "/sopacaracol.jpg" },
     { nombre: "Sopa Marinera", precio: "L 200", descripcion: "Sopa de mariscos cremosa y llena de sabor.", imagen: "/marinera.jpg" },
+    { nombre: "Tapado", precio: "L 200", descripcion: "Tradicional tapado hondureño preparado con una mezcla de mariscos y carnes.", imagen: "/tapado.jpg" },
   ],
 } as const;
 
@@ -92,7 +95,7 @@ export default function Menu() {
                 </div>
                 <div className="p-3 d-flex flex-column">
                   <h5 className="fw-bold mb-2">{plato.nombre}</h5>
-                  <p className="text-light-emphasis mb-2">{plato.descripcion}</p>
+<p className="descripcion mb-2">{plato.descripcion}</p>
                   <p className="fw-semibold precio">{plato.precio}</p>
                 </div>
               </div>
